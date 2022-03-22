a9477166fbacf067b700647c8ea2351d4d7236ee
chongpeiyian <chongpeiyian@yahoo.com>
2022-03-22 17:27:22 +0800

    Assignment 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSliderModule} from '@angular/material/slider';
 
 import { AppComponent } from './app.component';
 
@@ -64,6 +65,7 @@ import { LoginComponent } from './login/login.component';
     MatSlideToggleModule,
     MatCheckboxModule,
     MatProgressSpinnerModule,
+    MatSliderModule,
     ReactiveFormsModule
   ],
   entryComponents: [
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs/operators';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { Comment } from '../shared/comment';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as internal from 'assert';
+
 
 
 @Component({
@@ -12,17 +16,37 @@ import { Location } from '@angular/common';
   styleUrls: ['./dishdetail.component.scss']
 })
 export class DishdetailComponent implements OnInit {
-  
+  @ViewChild('fform') commentFormDirective;
+
+  commentForm: FormGroup;
+  comment: Comment;
   dish: Dish;
   strID: String;
   dishIds: string[];
   prev: string;
   next: string;
 
+  ratingDefault = 5;
+
+  formErrors = {
+    'author': '',
+    'comment': ''
+  };
+
+  validationMessages = {
+    'author': {
+      'required':      'Name is required.',
+      'minlength':     'Name must be at least 2 characters long.',
+    },
+    'comment': {
+      'required':      'Comment is required.',
+    }
+  };
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private fb: FormBuilder) { }
 
   ngOnInit() {
     // const id= +this.route.snapshot.params['id'];
@@ -35,6 +59,8 @@ export class DishdetailComponent implements OnInit {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
     .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+
+    this.createForm();
   }
 
   goBack(): void {
@@ -45,6 +71,56 @@ export class DishdetailComponent implements OnInit {
     const index = this.dishIds.indexOf(dishId);
     this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
     this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
+  }
+
+  createForm():void {
+    this.commentForm = this.fb.group({
+      author: ['', [Validators.required, Validators.minLength(2)] ],
+      rating: this.ratingDefault,
+      comment: ['', [Validators.required] ]
+    });
+
+    this.commentForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged(); // (re)set validation messages now
+  }
+
+  onSubmit() {
+    const today = new Date(Date.now());
+    this.comment = this.commentForm.value;
+    this.comment.date = today.toISOString();
+    console.log(this.comment);
+
+    this.dish.comments.push(this.comment)
+
+    this.commentForm.reset({
+      author: '',
+      rating: this.ratingDefault,
+      comment: ''
+    });
+    this.commentFormDirective.resetForm();
+    this.createForm();
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.commentForm) { return; }
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        // clear previous error message (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
   }
 
 }
