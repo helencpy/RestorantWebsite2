49e259d8cd7eafa00ed4a717e593ce16c69fa3e2
chongpeiyian <chongpeiyian@yahoo.com>
2022-03-25 22:37:22 +0800

    Directives

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -8,11 +8,11 @@ export class HighlightDirective {
   constructor(private el: ElementRef,
     private renderer: Renderer2) { }
 
-  @HostListener('mouseenter') onMouseEnter() {
+  @HostListener('mouseenter') onmouseenter() {
     this.renderer.addClass(this.el.nativeElement, 'highlight');
   }
 
-  @HostListener('mouseleave') onMouseLeave() {
+  @HostListener('mouseleave') onmouseleave() {
     this.renderer.removeClass(this.el.nativeElement, 'highlight');
   }
 }
