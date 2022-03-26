911e7a67138c5ec6d316bccfde8bfb7c5e323df2
chongpeiyian <chongpeiyian@yahoo.com>
2022-03-26 15:33:06 +0800

    Assignment4

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,15 +1,34 @@
 import { Injectable } from '@angular/core';
 import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
-import { delay } from 'rxjs/operators';
+import { delay, map, catchError } from 'rxjs/operators';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { baseURL } from '../shared/baseurl';
+import { ProcessHTTPMsgService } from './process-httpmsg.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LeaderService {
 
-  constructor() { }
+  constructor(private http: HttpClient,
+    private processHTTPMsgService: ProcessHTTPMsgService) { }
+
+    getLeaders(): Observable<Leader[]> {
+      return this.http.get<Leader[]>(baseURL + 'leadership')
+      .pipe(catchError(this.processHTTPMsgService.handleError));
+    }
+  
+    getLeader(id: string): Observable<Leader> {    
+      return this.http.get<Leader>(baseURL + 'leadership/' + id)
+      .pipe(catchError(this.processHTTPMsgService.handleError));
+    }
+  
+    getFeaturedLeader(): Observable<Leader> {
+      return this.http.get<Leader[]>(baseURL + 'leadership?featured=true').pipe(map(leader => leader[0]))
+      .pipe(catchError(this.processHTTPMsgService.handleError));
+    }
 
   // getLeaders(): Promise<Leader[]> {
   //   return Promise.resolve(LEADERS);
@@ -56,15 +75,15 @@ export class LeaderService {
   //   return of(LEADERS.filter((leader) => leader.featured)[0]).pipe(delay(2000)).toPromise();
   // }
 
-  getLeaders(): Observable<Leader[]> {
-    return of(LEADERS).pipe(delay(2000));
-  }
+  // getLeaders(): Observable<Leader[]> {
+  //   return of(LEADERS).pipe(delay(2000));
+  // }
 
-  getLeader(id: string): Observable<Leader> {
-    return of(LEADERS.filter((leader) => (leader.id === id))[0]).pipe(delay(2000));
-  }
+  // getLeader(id: string): Observable<Leader> {
+  //   return of(LEADERS.filter((leader) => (leader.id === id))[0]).pipe(delay(2000));
+  // }
 
-  getFeaturedLeader(): Observable<Leader> {
-    return of(LEADERS.filter((leader) => leader.featured)[0]).pipe(delay(2000));
-  }
+  // getFeaturedLeader(): Observable<Leader> {
+  //   return of(LEADERS.filter((leader) => leader.featured)[0]).pipe(delay(2000));
+  // }
 }
--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -2,15 +2,35 @@ import { Injectable } from '@angular/core';
 import { Promotion } from '../shared/promotion';
 import { PROMOTIONS } from '../shared/promotions';
 import { Observable, of } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { delay, map, catchError } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { baseURL } from '../shared/baseurl';
+import { ProcessHTTPMsgService } from './process-httpmsg.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PromotionService {
 
-  constructor() { }
+  constructor(private http: HttpClient,
+    private processHTTPMsgService: ProcessHTTPMsgService) { }
 
+    getPromotions(): Observable<Promotion[]> {
+      return this.http.get<Promotion[]>(baseURL + 'promotions')
+        .pipe(catchError(this.processHTTPMsgService.handleError));
+    }
+  
+    getPromotion(id: string): Observable<Promotion> {
+      return this.http.get<Promotion>(baseURL + 'promotions/' + id)
+        .pipe(catchError(this.processHTTPMsgService.handleError));
+  
+    }
+  
+    getFeaturedPromotion(): Observable<Promotion> {
+      return this.http.get<Promotion[]>(baseURL + 'promotions?featured=true').pipe(map(promotion => promotion[0]))
+        .pipe(catchError(this.processHTTPMsgService.handleError));
+    }
+    
   // getPromotions(): Promise<Promotion[]> {
   //   return Promise.resolve(PROMOTIONS);
   // }
@@ -58,17 +78,17 @@ export class PromotionService {
   //   return of(PROMOTIONS.filter((promotion) => promotion.featured)[0]).pipe(delay(2000)).toPromise();
   // }
 
-  getPromotions(): Observable<Promotion[]> {
-    return of(PROMOTIONS).pipe(delay(2000));
-  }
+  // getPromotions(): Observable<Promotion[]> {
+  //   return of(PROMOTIONS).pipe(delay(2000));
+  // }
 
-  getPromotion(id: string): Observable<Promotion> {
-    return of(PROMOTIONS.filter((promo) => (promo.id === id))[0]).pipe(delay(2000));
+  // getPromotion(id: string): Observable<Promotion> {
+  //   return of(PROMOTIONS.filter((promo) => (promo.id === id))[0]).pipe(delay(2000));
 
-  }
+  // }
 
-  getFeaturedPromotion(): Observable<Promotion> {
-    return of(PROMOTIONS.filter((promotion) => promotion.featured)[0]).pipe(delay(2000));
-  }
+  // getFeaturedPromotion(): Observable<Promotion> {
+  //   return of(PROMOTIONS.filter((promotion) => promotion.featured)[0]).pipe(delay(2000));
+  // }
   
 }
